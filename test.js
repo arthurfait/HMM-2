7153d8c31f2aa2581fb9929ca778c707878bd0e8
Jimin Park <pjm950@gmail.com>
2013-03-30 12:05:33 +0900

    There's no need to write back.

--- test.js
+++ test.js
@@ -5,5 +5,3 @@ var hmm = HMM.create(fs.readFileSync('hmm.txt').toString());
 
 console.log('Generating...');
 for(var i=0;i<100;i++) console.log(hmm.generate('$', ~~(Math.random()*4+4)).slice(0,-1));
-
-fs.writeFileSync('hmm.txt', hmm.toString());
